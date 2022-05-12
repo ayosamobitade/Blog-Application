3e66c554c5feb28adc5c7699d19027f25da4dca2
UncleAyo <ayosamobitade@gmail.com>
2022-05-12 11:34:29 +0100

    worked on the pages app

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import HomePageView
+
+
+class HomepageTests(SimpleTestCase):
+   
+    def setUp(self):
+        url = reverse('homepage')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'pages/index.html')
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, 'Let')
+    
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(
+            self.response, 'Hi there! I should not be on the page.')
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+            )
--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.HomePageView, name = "homepage")
+    path("", views.HomePageView.as_view(), name = "homepage")
 ]

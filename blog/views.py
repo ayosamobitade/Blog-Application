f479c41467d7a75842c92f779ccaa15f1bab2bc4
UncleAyo <ayosamobitade@gmail.com>
2022-05-12 10:28:08 +0100

    updated blog.views

--- blog/views.py
+++ blog/views.py
@@ -14,14 +14,14 @@ class PostListView(ListView):
 
 
 
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, 
-    slug = post,
-    status = "published",
-    publish_year = year,
-    published_month = month,
-    publish_day = day)
 
-    return render(request, "blog/post/detail.html",
-    {"post":post}
-    )
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+    status='published',
+    publish__year=year,
+    publish__month=month,
+    publish__day=day)
+    return render(request,
+    'blog/post/detail.html',
+    {'post': post})

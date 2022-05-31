6e838e1871a0b19fe2c81655def75cf5d0117ed1
UncleAyo <ayosamobitade@gmail.com>
2022-05-31 10:32:53 +0100

    changed database to postgresql

--- myblog/settings.py
+++ myblog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-cac2)!on(7lx-%ku7xro3105nlk)*(k1&c6f!#69l+)s@$xd$_'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',
         'USER': 'blog',
-        'PASSWORD' : "BAbatunde14,."
+        'PASSWORD' : 'BAbatunde14,.',
     }
 }
 

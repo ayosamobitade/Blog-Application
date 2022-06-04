8a440389e90ef5d380c2eda122191dcc512c7296
UncleAyo <ayosamobitade@gmail.com>
2022-06-04 08:33:40 +0100

    test

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render, get_object_or_404
 from taggit.models import Tag
 
+
 # Create your views here.
 from .models import Post, Comment
 from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
+from django.contrib.postgres.search import SearchVector
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search = SearchVector('title', 'body'),
+            ).filter(search = query)
+    return render(request, 'blog/post/search.html',{
+        'form':form,
+        'query':query,
+        'results':results
+    })
+
 
 
 class PostListView(ListView):

4f816c6d7a732501d9bdfc5202e3b6594a5e4e27
UncleAyo <ayosamobitade@gmail.com>
2022-06-08 17:39:45 +0100

    some updates

--- blog/views.py
+++ blog/views.py
@@ -59,6 +59,8 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+        
     return render(request, 'blog/post/detail.html',{
         'post': post,
         'comments': comments,

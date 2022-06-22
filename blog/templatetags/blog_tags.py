c0a8c940b87de9eb718b96cb26d08f03ee6829c0
UncleAyo <ayosamobitade@gmail.com>
2022-06-22 12:59:21 +0100

    some updates

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,11 +1,17 @@
 from django import template
 from ..models import Post
+from django.db.models import Count
 
 register = template.Library()
 
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+def get_most_commented_posts(count = 5):
+    return Post.published.annotate(
+        total_comments = Count('comments')
+    ).order_by('-total_comments')[:count]
 
 @register.inclusion_tag('blog/post/latest_posts.html')
 def show_latest_posts(count = 5):

016ed695310210783a29607be4fd7407cd105428
UncleAyo <ayosamobitade@gmail.com>
2022-06-22 20:04:54 +0100

    some updates

--- blog/forms.py
+++ blog/forms.py
@@ -21,20 +21,3 @@ class CommentForm(forms.ModelForm):
 class SearchForm(forms.Form):
     query = forms.CharField()
 
-def post_search(request):
-    form = SearchForm()
-    query = None
-    results = []
-    if 'query' in request.GET:
-        form = SearchForm(request.GET)
-        if form.is_valid():
-            query = form.cleaned_data['query']
-            results = Post.published.annotate(
-                search = SearchVector('title', 'body'),
-            ).filter(search = query)
-    return render(request, 'blog/post/search.html', 
-    {
-        'form':form,
-        'query':query,
-        'results':results
-    })

e4ba397231707b6482407260cf0565a3b2e16139
UncleAyo <ayosamobitade@gmail.com>
2022-06-22 13:03:54 +0100

    some updates

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -7,7 +7,7 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
-
+@register.simple_tag
 def get_most_commented_posts(count = 5):
     return Post.published.annotate(
         total_comments = Count('comments')

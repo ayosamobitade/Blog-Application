9e435a0b127a78d862176a18392868e0377728cc
UncleAyo <ayosamobitade@gmail.com>
2022-06-22 13:18:34 +0100

    some updates

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+import markdown
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -17,3 +19,7 @@ def get_most_commented_posts(count = 5):
 def show_latest_posts(count = 5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts':latest_posts}
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))

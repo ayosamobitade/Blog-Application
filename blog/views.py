3bea004e872e8bddc6d8f8b69c43ee4ef455f5a2
UncleAyo <ayosamobitade@gmail.com>
2022-08-21 18:55:25 +0100

    some updates

--- blog/views.py
+++ blog/views.py
@@ -64,6 +64,10 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in = post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tag=Count('tag')).order_by('-same_tags', '-publish')[:4]
 
     return render(request, 'blog/post/detail.html',{
         'post': post,

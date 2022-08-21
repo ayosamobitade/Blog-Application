843664f4f3c92a1966549c8a005539590b34d895
UncleAyo <ayosamobitade@gmail.com>
2022-08-21 16:44:54 +0100

    some updates

--- blog/views.py
+++ blog/views.py
@@ -19,9 +19,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search = SearchVector('title', 'body'),
-            ).filter(search = query)
+                search = search_vector, 
+                rank = SearchRank(search_vector, search_query)
+                ).filter(search = search_query).order_by('rank')
     return render(request, 'blog/post/search.html',{
         'form':form,
         'query':query,

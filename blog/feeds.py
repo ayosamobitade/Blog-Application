9d5f360ebeff716684da0c46592143da8fae0a23
UncleAyo <ayosamobitade@gmail.com>
2022-09-02 16:18:20 +0100

    some updates

--- blog/feeds.py
+++ blog/feeds.py
@@ -1,18 +1,13 @@
-from django.contrib.syndication.views import Feed
-from django.template.defaultfilters import truncatewords
-from django.urls import reverse_lazy
+from django.contrib.sitemaps import Sitemap
 from .models import Post
 
-class latestPostsFeed(Feed):
-    title = 'My blog'
-    link = reverse_lazy('blog:post_list')
-    description ='New posts of my blog'
+
+class PostSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.9
 
     def items(self):
-        return Post.published.all()[:5]
+        return Post.published.all()
 
-    def item_title(self, item):
-        return item.title
-
-    def item_description(self, item):
-        return truncatewords(item.body, 30)
+    def lastmod(self, obj):
+        return obj.updated

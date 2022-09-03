6ba242fa136fdb52e266e6faa01a19a3101bb089
UncleAyo <ayosamobitade@gmail.com>
2022-09-03 23:17:56 +0100

    some updates

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 
-from blog.feeds import latestPostsFeed
+from blog.feeds import LatestPostsFeed
 from . import views
 
 app_name = 'blog'
@@ -11,6 +11,6 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name = 'post_share'),
-    path('feed/', latestPostsFeed(), name = 'post_feed'),
+    path('feed/', LatestPostsFeed(), name = 'post_feed'),
     path('search/', views.post_search, name='post_search'),
 ]
